0af3a94764909c98ac2dc61fa0ea98a445ec6a48
Amir1235 <42092550+Amir1235@users.noreply.github.com>
2025-04-14 08:32:50 +0200

    auktionsdetaljer

--- frontend/src/auction-detail.ts
+++ frontend/src/auction-detail.ts
@@ -15,6 +15,17 @@ async function fetchAuctionDetails() {
       const response = await axios.get(`http://localhost:3000/auctions/${auctionId}`, {
         withCredentials: true,
       });
+      const auction = response.data;
+
+      const auctionTitle = document.getElementById("Title");
+      const auctionDescription = document.getElementById("Description");
+      const auctionStartPrice = document.getElementById("StartPrice");
+      const auctionEndTime = document.getElementById("EndTime");
+  
+      if (auctionTitle) auctionTitle.textContent = auction.title;
+      if (auctionDescription) auctionDescription.textContent = auction.description;
+      if (auctionStartPrice) auctionStartPrice.textContent = `Startpris: ${auction.startPrice} kr`;
+      if (auctionEndTime) auctionEndTime.textContent = `Slutar: ${new Date(auction.endDate).toLocaleString()}`;
     } catch (error) {
         console.error("Failed to fetch auction details:", error);
       }
